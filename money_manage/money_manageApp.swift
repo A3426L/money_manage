c499e8988bdd669d25322aa0e553564ca7b23201
A3426L <mikan7738@gmail.com>
2025-04-25 17:59:59 +0900

    Initial Commit

---
+++ money_manage/money_manageApp.swift
@@ -0,0 +1,17 @@
+//
+//  money_manageApp.swift
+//  money_manage
+//
+//  Created by aru on 2025/04/25.
+//
+
+import SwiftUI
+
+@main
+struct money_manageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
