95450c111f2c1cfa26f0ea6b3e88c0c957cb0327
A3426L <mikan7738@gmail.com>
2025-04-29 00:50:28 +0900

    [edit]circle slider

---
+++ money_manage/CircleSlider.swift
@@ -0,0 +1,8 @@
+//
+//  CircleSlider.swift
+//  money_manage
+//
+//  Created by aru on 2025/04/26.
+//
+
+import Foundation
---
+++ money_manage/tripleSlider.swift
@@ -0,0 +1,8 @@
+//
+//  tripleSlider.swift
+//  money_manage
+//
+//  Created by aru on 2025/04/26.
+//
+
+import Foundation
