718ef87d7da505eb3d01f18a9a8f15e1b3cfac1c
A3426L <mikan7738@gmail.com>
2025-05-01 10:53:47 +0900

    [add]move animation

---
+++ money_manage/Animation.swift
@@ -0,0 +1,18 @@
+//
+//  Animetion.swift
+//  money_manage
+//
+//  Created by aru on 2025/05/01.
+//
+
+import SwiftUI
+
+struct Animetion: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Animetion()
+}
--- money_manage/tripleSlider.swift
+++
@@ -1,8 +0,0 @@
-//
-//  tripleSlider.swift
-//  money_manage
-//
-//  Created by aru on 2025/04/26.
-//
-
-import Foundation
