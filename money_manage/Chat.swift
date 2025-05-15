8594ee6996b16eaa089ee83de215d89c56911c4e
A3426L <mikan7738@gmail.com>
2025-05-15 17:00:41 +0900

    update: add animated custom button component (#9)

---
+++ money_manage/Chat.swift
@@ -0,0 +1,56 @@
+//
+//  Chat.swift
+//  money_manage
+//
+//  Created by aru on 2025/05/13.
+//
+
+import SwiftUI
+
+struct Chat: View {
+    @State var currentIndex: Int = 0
+    @State var textlines: [String] = ["一週間の予測を伝えるよ！","今週はあんま良くないよ","もっと頑張ろう"]
+    var body: some View {
+        Text(textlines[currentIndex])
+            .font(.largeTitle)
+            .frame(height: 100, alignment: .leading)
+            .frame(maxWidth: .infinity)
+            .border(Color.black)
+            .background(Color.yellow.opacity(0.2))
+            .cornerRadius(12)
+            .padding()
+            .onTapGesture {
+                if currentIndex < textlines.count - 1 {
+                    currentIndex += 1
+                }
+            }
+        Spacer()
+        Grid(alignment: .bottom,horizontalSpacing: 50, verticalSpacing: 10){
+            GridRow{
+                CustomButton(label: "一週間の予測",action: {
+                    currentIndex = 0
+                    textlines = ["一週間の予測を伝えるよ！","今週はあんま良くないよ","もっと頑張ってね"]
+                })
+                
+                CustomButton(label: "明日の予測",action: {
+                    currentIndex = 0
+                    textlines = ["明日の予測を教えるよ！","明日は5,000円使うらしいで","程々にね"]
+                })
+            }
+            
+            GridRow{
+                CustomButton(label: "お話ししよ〜",action: {
+                    currentIndex = 0
+                    textlines = ["お話し？","舐めてんのか？","暇人が!働け！"]
+                })
+            }
+        }
+        .frame(maxWidth: .infinity)
+        .border(Color.green)
+    }
+    }
+
+
+#Preview {
+    Chat()
+}
--- money_manage/MainView.swift
+++ money_manage/MainView.swift
@@ -9,70 +9,12 @@ import SwiftUI
 
 struct MainView: View {
     @State private var text:String = "Hello!"
-    @State private var textlines:[String] = ["こんにちは！","今日はいい天気ですね。","hahaha"]
-    @State private var currentIndex:Int = 0
     var body: some View {
         VStack{
             Animation(aligment: .topCenter, fit:.contain)
                 .frame(height: 300)
                 .border(Color.blue)
-            Text(textlines[currentIndex])
-                .font(.largeTitle)
-                .frame(height: 100, alignment: .leading)
-                .frame(maxWidth: .infinity)
-                .border(Color.black)
-                .background(Color.yellow.opacity(0.2))
-                .cornerRadius(12)
-                .padding()
-                .onTapGesture {
-                    if currentIndex < textlines.count - 1 {
-                        currentIndex += 1
-                    }
-                }
-            Spacer()
-            Grid{
-                GridRow{
-                    Button {
-                        currentIndex = 0
-                        textlines = ["一週間の予測を伝えるよ！","今週はあんま良くないよ","もっと頑張れよてめえ"]
-                    } label: {
-                        Text("一週間の予測")
-                    }
-                    .padding()
-                    .accentColor(Color.white)
-                    .background(Color.blue)
-                    .cornerRadius(.infinity)
-                    .shadow(color: Color.black, radius: 0, x: 0, y: 5)
-                    
-                    Button {
-                        currentIndex = 0
-                        textlines = ["明日の予測を教えるよ！","明日は5,000円使うらしいで","程々にしとけよワレ"]
-                    } label: {
-                        Text("明日の予測")
-                    }
-                    .padding()
-                    .accentColor(Color.white)
-                    .background(Color.blue)
-                    .cornerRadius(.infinity)
-                    .shadow(color: Color.black, radius: 0, x: 0, y: 6)
-                }
-                GridRow{
-                    Button {
-                        currentIndex = 0
-                        textlines = ["お話し？","舐めてんのか？","暇人が!働け！"]
-                    } label: {
-                        Text("お話ししよ〜")
-                    }
-                    .padding()
-                    .accentColor(Color.white)
-                    .background(Color.blue)
-                    .cornerRadius(.infinity)
-                    .shadow(color: Color.black, radius: 0, x: 0, y: 5)
-                    
-                }
-            }
-            .frame(maxWidth: .infinity)
-            .border(Color.green)
+            Chat()
         }
     }
 }
