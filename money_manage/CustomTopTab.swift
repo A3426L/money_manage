fac5c70256af4e0e53616a1c1091a365de0a8826
A3426L <mikan7738@gmail.com>
2025-05-20 18:47:53 +0900

    feat: add top TabView

---
+++ money_manage/CustomTopTab.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTopTab.swift
+//  money_manage
+//
+//  Created by aru on 2025/05/20.
+//
+
+import SwiftUI
+
+struct CustomTopTab: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomTopTab()
+}
