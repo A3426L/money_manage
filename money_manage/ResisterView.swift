1b434a3709400d7538f45f826be214cf5cc072cb
A3426L <mikan7738@gmail.com>
2025-05-22 20:56:02 +0900

    update:circle slider  View

--- money_manage/ResisterView.swift
+++ money_manage/ResisterView.swift
@@ -8,18 +8,28 @@
 import SwiftUI
 
 struct ResisterView: View {
+    @State var CurrentValue: String = ""
+    @State var display_array = ["5000","2000","3000","4000","5000"]
     var body: some View {
         GeometryReader { geometry in
-            let center = CGPoint(x: geometry.size.width/2, y: geometry.size.height + 100)
+            let center = CGPoint(x: geometry.size.width/2, y: geometry.size.height + 80)
             Animation(aligment: .topCenter)
             ZStack{
                 GridBackground(spacing: 20)
                 VStack{
                     ZStack{
-                        CircleSlider(radius: geometry.size.width + 100 )
+                        CircleSlider(CurrentValue: $CurrentValue, display_array: $display_array, radius: geometry.size.width + 120)
                                 .position(center)
-                        CircleSlider(radius: geometry.size.width)
-                                .position(center)
+                                .onTapGesture {
+                                    print($CurrentValue.wrappedValue)
+                                }
+                        
+//                        CircleSlider(radius: geometry.size.width )
+//                                .position(center)
+//                        
+//                        CircleSlider(radius: geometry.size.width - 120 )
+//                                .position(center)
+
                     }
                 }
             }
