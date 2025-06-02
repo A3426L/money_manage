f3eae5abb83400346399254d72d8fe38fa121187
A3426L <mikan7738@gmail.com>
2025-06-02 19:30:21 +0900

    feat : delete one character

--- money_manage/ResisterView.swift
+++ money_manage/ResisterView.swift
@@ -11,35 +11,16 @@ struct ResisterView: View {
     @State var currentValue: Double = 5000
     @State var currentValue2: Double = 500
     @State var display_array = ["1000","2000","3000","4000","5000","6000","7000","8000","9000","10000"]
+    
+    @State var displayText: String = ""
+    @State var isYenAdded: Bool = false
     var body: some View {
         
-//        GeometryReader { geometry in
-//            let center = CGPoint(x: geometry.size.width/2, y: geometry.size.height + 80)
-//            Animation(aligment: .topCenter)
-//        }
         VStack{
             Animation(aligment: .topCenter)
-            
-            ZStack{
-                CircularPicker(value: $currentValue,
-                               minValue: 0.0,
-                               maxValue: 10000,
-                               tickCount: 11,
-                               minorTicksPerMajor: 0,
-                               stepValue: 1000)
-                .frame(width: 400, height: 500)
-                CircularPicker(value: $currentValue2,
-                               minValue: 0.0,
-                               maxValue: 1000,
-                               tickCount: 11,
-                               minorTicksPerMajor: 0,
-                               stepValue: 100)
-                .frame(width: 200, height: 200)
-                
-                Text(String(Int(currentValue + currentValue2)))
-   
-            }
-            .frame(width: 400,height: 50)
+            Text(displayText + (isYenAdded ? " 円" : ""))
+            MoneyInputView(displayText: $displayText, isYenAdded: $isYenAdded)
+                .frame(height: 500)
 
         }
     }
